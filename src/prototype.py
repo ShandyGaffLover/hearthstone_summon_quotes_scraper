a7ffc575016b3f5b2ee3f30f8b4c44192861fbae
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2019-02-25 21:51:38 +0900

    [update]Webスクレイピングしたカード名とURLをDictionaryに格納するように修正

--- src/prototype.py
+++ src/prototype.py
@@ -6,5 +6,10 @@ res = requests.get(url)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "html.parser")
 
+dict = {}
 for link in soup.find_all('a'):
-    print(link.get('href'))
+    title = link.get('title')
+    href  = link.get('href')
+    if(title != None):
+        dict[title] = href
+print(dict.keys())
