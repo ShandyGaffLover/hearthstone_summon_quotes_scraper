8df569d0ed51e95178540e68df4f6ada9b037275
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2019-02-26 10:24:54 +0900

    [update]拡張名を引数にカード名とリンクの辞書を返却する関数を作成

--- src/prototype.py
+++ src/prototype.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://hearthstone.gamepedia.com/The_Boomsday_Project'
-res = requests.get(url)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "html.parser")
 
-dict = {}
-for link in soup.find_all('a'):
-    if(link.find('img')==None):
-        continue
-    title = link.get('title')
-    href  = link.get('href')
-    if(title != None):
-        dict[title] = href
+def create_dictionary_of_card_name_and_its_link(expansion_name: str) -> dict:
+    url = 'https://hearthstone.gamepedia.com/' + expansion_name
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+    dict = {}
+    for link in soup.find_all('a'):
+        if(link.find('img')==None):
+            continue
+        title = link.get('title')
+        href  = link.get('href')
+        if(title != None):
+            dict[title] = href
+    return dict
+
+
+
+expansion_name = 'The_Boomsday_Project'
+dict = create_dictionary_of_card_name_and_its_link(expansion_name)
 print(dict.keys())
