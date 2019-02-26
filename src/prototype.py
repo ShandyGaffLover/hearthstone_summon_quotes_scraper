265398482cf0e0f0692c752362462830cbdc76c9
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2019-02-26 10:14:07 +0900

    [update]Webスクレイピングの対象をimgタグを子要素に持つaタグに修正

--- src/prototype.py
+++ src/prototype.py
@@ -8,6 +8,8 @@ soup = BeautifulSoup(res.text, "html.parser")
 
 dict = {}
 for link in soup.find_all('a'):
+    if(link.find('img')==None):
+        continue
     title = link.get('title')
     href  = link.get('href')
     if(title != None):
