39c4430ba75cf392a02672a35bb6a5b1f2128b5f
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2019-02-26 10:58:46 +0900

    [update]各ミニオンの召喚時セリフを抽出するように修正

--- src/prototype.py
+++ src/prototype.py
@@ -18,7 +18,22 @@ def create_dictionary_of_card_name_and_its_link(expansion_name: str) -> dict:
     return dict
 
 
+def extract_summon_quotes(href: str) -> str:
+    url = 'https://hearthstone.gamepedia.com/' + href
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+    tag_written_about_quotes = soup.find('dl')
+    try:
+        return tag_written_about_quotes.find('dd').find('i').string
+    except AttributeError:
+        return None
+    
 
 expansion_name = 'The_Boomsday_Project'
 dict = create_dictionary_of_card_name_and_its_link(expansion_name)
-print(dict.keys())
+summon_quotes = {}
+for card in dict.items():
+    summon_quotes[card[0]]=extract_summon_quotes(card[1])
+print(summon_quotes.values())
+    
